de34b71dbba94d826cec70c1c64c03333f6bfcda
Pyxsys <barras.pierre.a@gmail.com>
2021-01-31 11:06:31 -0500

    config for db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 
+const config = {
+    views: 'Views',                         //Sets views directory
+    db: {
+        url: 'mongo://localhost/BikeErpDB', //Connect to local database "BikeErpDB"
+        type: 'mongo',
+        onError: (err) => {
+            console.log('Error: DB connection failed.')
+        }
+    }
+}
+
 //gen and set app instnace
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express(config);
+const PORT = process.env.PORT || 5000;  //Get open port (default to 5000)
 
 
 //begin listening
