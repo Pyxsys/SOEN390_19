d5aa7c20847de408986558366e2ce08641781ebe
nirmalrandhawa <nirmal_864@hotmail.com>
2021-02-09 15:58:20 -0500

    #36 added files for 3 models

---
+++ Server/src/Models/Client.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+// Schema
+const business_schema = mongoose.Schema({
+    
+});
+
+module.exports = mongoose.model('Business', business_schema);
---
+++ Server/src/Models/Supplier.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+// Schema
+const supplier_schema = mongoose.Schema({
+    
+});
+
+module.exports = mongoose.model('Supplier', supplier_schema);
