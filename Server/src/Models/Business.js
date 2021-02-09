e1810f26c1ed806449bae1f6cd54331eb87e4197
nirmalrandhawa <nirmal_864@hotmail.com>
2021-02-09 14:50:08 -0500

    added business model #36

---
+++ Server/src/Models/Business.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Schema
+const business_schema = mongoose.Schema({
+    business:       {type: String, required: true},
+    clients:       {type: String, required: true},
+    suppliers:          {type: String, required: true}
+});
+
+module.exports = mongoose.model('Business', Business);
