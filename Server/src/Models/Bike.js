5447f2f510eb312a38055601d3edcfe602a57e1c
nathanziri <nathanziri@hotmail.com>
2021-02-11 13:36:27 -0500

    Added fields database fields added
    Added fields for bike and bike parts databases

--- Server/src/Models/Bike.js
+++ Server/src/Models/Bike.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 // Schema
 const bike_schema = mongoose.Schema({
     internalId:       {type: String, required: true},
+    bikeID:           {type: String, required: true},//potential parts include: handlebars, front fork, frame, pedal, seat, tires
+    price:            {type: Number, required: true},
+    number_Of_Units:  {type: Number, required: true},
+    provider:         {type: String, required: true},
+    type:             {type: String, required: true}
     //TODO add attributes
 });
 
--- Server/src/Models/BikePart.js
+++ Server/src/Models/BikePart.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 // Schema
 const part_schema = mongoose.Schema({
     internalId:       {type: String, required: true},
+    partType:             {type: String, required: true},//potential parts include: handlebars, front fork, frame, pedal, seat, tires
+    price:            {type: Number, required: true},
+    number_Of_Units:  {type: Number, required: true},
+    provider:         {type: String, required: true}
     //TODO add attributes
 });
 
