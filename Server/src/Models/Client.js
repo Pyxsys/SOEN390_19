073171cc9879331df0c07754e94481459aeba933
nirmalrandhawa <nirmal_864@hotmail.com>
2021-02-11 17:13:11 -0500

    #36 few changes to model

--- Server/src/Models/Client.js
+++ Server/src/Models/Client.js
@@ -1,10 +1,13 @@
 
 const mongoose = require('mongoose');
 const business_schema = require('./Business.js');
+var Business = mongoose.model('Business', business_schema);
+var Client = Business.discriminator('Client', client_schema);
 
   // Schema
   const client_schema = mongoose.Schema({
-    businessArr: [business_schema]
+    client_name: {type: String, required: true},
+    client_title: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Client', client_schema);
--- Server/src/Models/Supplier.js
+++ Server/src/Models/Supplier.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const business_schema = require('./Business.js');
+var Business = mongoose.model('Business', business_schema);
+var Supplier = Business.discriminator('Supplier', supplier_schema);
 
 
 // Schema
 const supplier_schema = mongoose.Schema({
-
-    businessArr: [business_schema]
+    description: {type: String, required: true}
 });
     
 
