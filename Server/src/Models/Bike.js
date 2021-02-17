e3972947c6a3b0018d78ee7a4cf8f17206e120d5
Pyxsys <barras.pierre.a@gmail.com>
2021-02-17 12:40:00 -0500

    Added bikeparts list to bike schema

--- Server/src/Models/Bike.js
+++ Server/src/Models/Bike.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bikePart = require('BikePart.js');
 
 // Schema
 const bike_schema = mongoose.Schema({
@@ -8,7 +9,11 @@ const bike_schema = mongoose.Schema({
     number_Of_Units:  {type: Number, required: true},
     provider:         {type: String, required: false},
 
-    BikeDocPath:      {type: String, required: true} //Path to .pdf file
+    BikeDocPath:      {type: String, required: true}, //Path to .pdf file
+    PartsList: {
+        type: [BikeParts],  //array of bikeParts
+        default: undefined  //default to undefined instead of empty array
+    }
 });
 
 module.exports.Bikes = mongoose.model('Bikes', bike_schema);
