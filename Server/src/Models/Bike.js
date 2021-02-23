12378ae9c6289083c56de1858de74474ff828f24
Pyxsys <barras.pierre.a@gmail.com>
2021-02-23 09:19:48 -0500

    added descriptive comments

--- Server/src/Models/Bike.js
+++ Server/src/Models/Bike.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
-const bikePart = require('BikePart.js');
 
 // Schema
 const bike_schema = mongoose.Schema({
     internalId:       {type: String, required: true},
     type:             {type: String, required: true},   //general descriptor eg. mountain bike
     price:            {type: Number, required: true},
-    number_Of_Units:  {type: Number, required: true},
-    provider:         {type: String, required: false},
+    numberOfUnits:    {type: Number, required: true},
+    provider:         {type: String, required: false},  //refer to supplier
 
     BikeDocPath:      {type: String, required: true},   //Path to .pdf file
     PartsList: {
-        type: [BikeParts],  //array of bikeParts
+        type: [String],     //array of bikeParts, refers to internal IDs
         default: undefined  //default to undefined instead of empty array
     }
 });
