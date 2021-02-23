332d0f05fa3650bea694183a6d19bd947e2004d8
Pyxsys <barras.pierre.a@gmail.com>
2021-02-23 16:07:37 -0500

    added catalogue subDocument

--- Server/src/Models/Supplier.js
+++ Server/src/Models/Supplier.js
@@ -3,10 +3,16 @@ const business_schema = require('./Business.js');
 var Business = mongoose.model('Business', business_schema);
 var Supplier = Business.discriminator('Supplier', supplier_schema);
 
+// catalogue Schema
+const catalogue_schema = mongoose.Schema({
+    internalId:        {type: String, required: true},   //Destinct from mongoDB ID, given by industry standard
+    availableUnits:    {type: Number, required: true}    //number of the unit available
+});
 
 // Schema
 const supplier_schema = mongoose.Schema({
-    description: {type: String, required: true}
+    description: {type: String, required: true},
+    catalogue: [catalogue_schema]
 });
     
 
