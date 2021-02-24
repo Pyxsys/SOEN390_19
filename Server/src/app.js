52269e3ee39dba4292a68533083920b0accd4081
SekayZ <kaysse.rachid@outlook.fr>
2021-02-24 12:02:59 -0500

    Setting up to pull

--- Server/src/app.js
+++ Server/src/app.js
@@ -19,10 +19,10 @@ app.use('/users', userRoute);                           // Enforce use of route 
 
 
 // Connect to mongoose mongoDB
-/* Uncomment for local host mongodb connection
+// Uncomment for local host mongodb connection
 var db_link = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
-//*/
-var db_link = 'mongodb://mongo:27017/kapp-docker-express';    //docker mongo container link
+
+/*var db_link = 'mongodb://mongo:27017/kapp-docker-express';*/    //docker mongo container link
 mongoose.connect(
     db_link,                                    // Target DB
     { useNewUrlParser: true },                  // Removes deprecation warning
