7f623271627b8d5883ce472cf2c244a9b264bfb1
Nirmal <nirmal_864@hotmail.com>
2021-03-02 17:31:27 -0500

    #66 changed number to datetime

--- Server/src/Models/Assembler.js
+++ Server/src/Models/Assembler.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 // Schema
 const assembler_schema = mongoose.Schema({
-    start: {type: Number, required: true},
-    end:       {type: Number, required: true},
-    task:          {type: String, required: true},
+    start: {type: Date, required: true},
+    end:   {type: Date, required: true},
+    task:  {type: String, required: true},
 });
 
 module.exports = mongoose.model('Assembler', assembler_schema);
