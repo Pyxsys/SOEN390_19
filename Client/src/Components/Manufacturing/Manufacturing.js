e210a8bbec16f937dce857d426408768d22abde4
Pyxsys <barras.pierre.a@gmail.com>
2021-03-17 15:44:10 -0400

    adding Manufacturing.js to appease git

---
+++ Client/src/Components/Manufacturing/Manufacturing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+function Manufacturing() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default Manufacturing;
