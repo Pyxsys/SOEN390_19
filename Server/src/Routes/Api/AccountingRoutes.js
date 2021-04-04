d7c2349cf638de9b7971ac8a0455a55fe649d6a6
Nirmal <nirmal_864@hotmail.com>
2021-04-04 19:23:25 -0400

    #16 #17 added post method

--- Server/src/Routes/Api/AccountingRoutes.js
+++ Server/src/Routes/Api/AccountingRoutes.js
@@ -6,23 +6,54 @@
 
 const express = require('express');
 const router = express.Router();
-//const Sale = require('../../Models/Sales.js');
+
+const Accounting = require('../../Models/Accounting');
 const Bike = require('../../Models/Bike.js');
 
 // Routes-------------------------
 // ------Accounting----------------
 
-/** PART - GET
+/** Accounting - GET
  *  Returns and displays all accounting information: saleID, bike name, number of bikes, total revenue
  */
 
  router.get('/' , async (req, res) => {
     try{
-        const sales = await Bike.Bikes.find(); // change to const sales = await Sale.Sales.find() once sales model is complete
+        const sales = await Accounting.Accounting.find(); // change to const sales = await Sale.Sales.find(); once sales model is complete
         res.json(sales);    //returns all found sales
     } catch(err){
         console.log(`> failed: ${err}`);
         res.status(500).json(`{message: failed at retrieval}`);
+    }
+});
+
+/** Accounting - Post
+ *  Adds accounting information to DB
+ */
+
+ router.post('/post', async (req, res) => {
+ 
+
+    // Create new accounting instance
+    const new_accounting = new Accounting.Accounting({
+        internalId:       req.body.internalId, //Distinct from mongoDB ID, given by industry standard
+        item:         req.body.item,   
+        price:            req.body.price,
+        numberOfUnits:    req.body.numberOfUnits,
+        client:         req.body.client
+    });
+    
+    console.log(new_accounting);
+    
+    // Save new accounting instance to db
+    try{
+        const saved_part = await new_accounting.save();
+        res.json(`{message: added part '${saved_part.internalId}' to DB}`);
+        console.log('> added part successfully.');
+    } catch(err) {
+        console.log(`> failed: ${err}`);
+        res.status(400).json({message: err});           // return error
+        console.log('> part not added.');
     }
 });
 
